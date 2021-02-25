0cccb8e388cd2927563105cd414287e96ce49c36
mczhuge <64179323+mczhuge@users.noreply.github.com>
2021-02-25 12:11:27 +0800

    Update run_eval.sh

--- run_eval.sh
+++ run_eval.sh
@@ -1,48 +1,50 @@
 # SOC evaluation 
 
+METHOD='ICON'
+
 python ./codes/soc_eval.py   \
-    --method  'ICON' \
+    --method  $METHOD \
     --dataset  'SOC' \
     --attr  'SOC-AC' \
 
 python  ./codes/soc_eval.py   \
-    --method  'ICON' \
+    --method   $METHOD \
     --dataset  'SOC' \
     --attr  'SOC-BO' \
 
 
 python  ./codes/soc_eval.py   \
-    --method  'ICON' \
+    --method   $METHOD \
     --dataset  'SOC' \
     --attr  'SOC-CL' \
 
 python  ./codes/soc_eval.py   \
-    --method  'ICON' \
+    --method   $METHOD \
     --dataset  'SOC' \
     --attr  'SOC-HO' \
 
 python  ./codes/soc_eval.py   \
-    --method  'ICON' \
+    --method   $METHOD \
     --dataset  'SOC' \
     --attr  'SOC-MB' \
 
 python  ./codes/soc_eval.py   \
-    --method  'ICON' \
+    --method  $METHOD \
     --dataset  'SOC' \
     --attr  'SOC-OC' \
 
 python  ./codes/soc_eval.py   \
-    --method  'ICON' \
+    --method   $METHOD \
     --dataset  'SOC' \
     --attr  'SOC-OV' \
     
 python  ./codes/soc_eval.py   \
-    --method  'ICON' \
+    --method   $METHOD \
     --dataset  'SOC' \
     --attr  'SOC-SC' \
 
 python  ./codes/soc_eval.py   \
-    --method  'ICON' \
+    --method   $METHOD \
     --dataset  'SOC' \
     --attr  'SOC-SO' \
 
